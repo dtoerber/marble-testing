f905e9e00c49a821205da85cd6019f7cf347326a
Dave Toerber <d.toerber@marrickdevelopmen.com>
2022-03-22 20:55:23 -0600

    initial version

---
+++ src/app/data.service.spec.ts
@@ -0,0 +1,75 @@
+import { TestBed } from '@angular/core/testing';
+import { DataService } from './data.service';
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime } from 'rxjs/operators';
+
+describe('DataService', () => {
+  let service: DataService;
+  let testScheduler: TestScheduler;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DataService);
+    testScheduler = new TestScheduler((actual, expected) =>
+      expect(actual).toEqual(expected)
+    );
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return getList$ default values', () => {
+    testScheduler.run(({ hot, expectObservable, cold }) => {
+      const expected = '(a|)';
+      const expectResult = { a: ['value1', 'value2', 'value3'] };
+      expectObservable(service.getList$).toBe(expected, expectResult);
+    });
+  });
+
+  it('should return the getCombineLastest$ default values', () => {
+    testScheduler.run(({ expectObservable }) => {
+      const expected = '(a|)';
+      const expectedResult = {
+        a: [
+          ['value1', 'value2', 'value3'],
+          [1, 2, 3],
+        ],
+      };
+      expectObservable(service.getCombineLatest$).toBe(
+        expected,
+        expectedResult
+      );
+    });
+  });
+
+  it('should return the getWithLatestFrom$ default values', () => {
+    testScheduler.run(({ expectObservable, cold }) => {
+      const expected = '(d|)';
+      const expectedResult = {
+        d: [
+          ['value1', 'value2', 'value3'],
+          [4, 5, 6],
+        ],
+      };
+      expectObservable(service.getWithLatestFrom$).toBe(
+        expected,
+        expectedResult
+      );
+    });
+  });
+
+  // This test runs synchronously.
+  it('generates the stream correctly', () => {
+    testScheduler.run((helpers) => {
+      const { cold, time, expectObservable, expectSubscriptions } = helpers;
+      const e1 = cold(' -a--b--c---|');
+      const e1subs = ' ^----------!';
+      const t = time(' ---|        '); // t = 3
+      const expected = ' -a-----c---|';
+
+      expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
+  });
+});
---
+++ src/app/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, combineLatest } from 'rxjs';
+import { withLatestFrom } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor() {}
+
+  getList$: Observable<string[]> = of(['value1', 'value2', 'value3']);
+
+  getNumbers1$: Observable<number[]> = of([1, 2, 3]);
+
+  getNumbers2$: Observable<number[]> = of([4, 5, 6]);
+
+  getNumbers3$: Observable<number[]> = of([7, 8, 9]);
+
+  getBooleans$: Observable<boolean> = of(false, false, true, false);
+
+  getCombineLatest$ = combineLatest([this.getList$, this.getNumbers1$]);
+
+  getWithLatestFrom$ = this.getList$.pipe(withLatestFrom(this.getNumbers2$));
+}
